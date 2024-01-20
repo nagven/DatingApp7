4b7bcd7339f6829df18441eb10271656b128efa2
Prasad <Prasad2009@hotmail.com>
2024-01-20 11:34:48 -0600

    End Of Section2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+using System.Reflection.Metadata.Ecma335;
+using API.Data;
+using API.Data.Migrations;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // api/users
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        return user;
+    }
+}
+
+
